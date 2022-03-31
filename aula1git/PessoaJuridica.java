a2b547075b804d1ed70de0a1d3a7bbdabfffef97
nadiabsouza <nadialuna.souza@gmail.com>
2022-03-31 00:50:38 -0300

    Primeiro commit

---
+++ aula1git/PessoaJuridica.java
@@ -0,0 +1,42 @@
+package fgabank;
+
+public class PessoaJuridica extends PessoaHerançaa
+{
+
+	private String cnpj;
+	private String segmento;
+	private String responsavel;
+
+	public PessoaJuridica() {
+	}
+	public PessoaJuridica(String cnpj, String segmento, String responsavel) {
+		super();
+		this.cnpj = cnpj;
+		this.segmento = segmento;
+		this.responsavel = responsavel;
+	}
+	public String getCnpj() {
+		return cnpj;
+	}
+	public void setCnpj(String cnpj) {
+		this.cnpj = cnpj;
+	}
+	public String getSegmento() {
+		return segmento;
+	}
+	public void setSegmento(String segmento) {
+		this.segmento = segmento;
+	}
+	public String getResponsavel() {
+		return responsavel;
+	}
+	public void setResponsavel(String responsavel) {
+		this.responsavel = responsavel;
+	}
+	
+}
+
+
+
+	
+	
